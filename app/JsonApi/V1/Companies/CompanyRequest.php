788daba25dc32bca9476ee82dc4efa01b4f5796e
Yoimel Rojas <yoimeldev@gmail.com>
2024-11-16 19:54:12 -0400

    feat: add unique validation rule for document_number in CompanyRequest; handle uniqueness during updates

--- app/JsonApi/V1/Companies/CompanyRequest.php
+++ app/JsonApi/V1/Companies/CompanyRequest.php
@@ -4,6 +4,7 @@ namespace App\JsonApi\V1\Companies;
 
 use App\Enums\CompanyStatus;
 use App\Enums\DocumentType;
+use Illuminate\Validation\Rule;
 use LaravelJsonApi\Laravel\Http\Requests\ResourceRequest;
 use LaravelJsonApi\Validation\Rule as JsonApiRule;
 use Illuminate\Validation\Rules\Enum;
@@ -17,10 +18,16 @@ class CompanyRequest extends ResourceRequest
      */
     public function rules(): array
     {
+        $uniqueRule = Rule::unique('companies', 'document_number');
+        
+        if ($this->isUpdating()) {
+            $uniqueRule->ignore($this->model());
+        }
+        
         return [
             'name' => ['required', 'string', 'max:255'],
             'document_type' => ['required', 'string', new Enum(DocumentType::class)],
-            'document_number' => ['required', 'string', 'max:255'],
+            'document_number' => ['required', 'string', 'max:255', $uniqueRule],
             'status' => ['required', 'string', new Enum(CompanyStatus::class)],
             'user' => ['required', JsonApiRule::toOne()],
             'activityTypes' => JsonApiRule::toMany(),
